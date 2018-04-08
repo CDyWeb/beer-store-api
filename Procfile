c9c8a83505755adfb9c5b8d2412682f19ace2255
Unknown <erwin.kooi@empire.ca>
2018-04-08 10:44:58 -0400

    Heroku deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn beer_store_api.wsgi --log-file -
--- beer_store_api/wsgi.py
+++ beer_store_api/wsgi.py
@@ -1,18 +1,14 @@
 """
-WSGI config for beer_store_api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os, sys
-
-
-sys.path.append('/home/djuta/webapps/beer_store_api/beer_store_api')
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "beer_store_api.settings"
+import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beer_store_api.settings")
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
